04dd016d711695c71e737d64f05175c6be405b2a
Tadas Sasnauskas <tadas@yoyo.lt>
2016-03-31 23:32:01 +0100

    Sample z80 code now both reads and writes to memory.. and it works

--- hello1.asm
+++ hello1.asm
@@ -4,10 +4,17 @@ org 0x0
 
 main:
     nop
+    ld a, 10
+    ld b, 20
+    add a, b
+    ld (result), a
     nop
     nop
     nop
     jp main
+
+result:
+    defb 0x0
 
 hello:
     defb "Hello world!", 0
